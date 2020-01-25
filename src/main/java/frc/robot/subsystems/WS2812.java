f74fa48fc3dbb6f196e4cd0916d55abf8f4b08aa
Mr. A <GabrielArtaud@comcast.net>
2020-01-24 21:28:07 -0500

    switched to tab size of 4

--- src/main/java/frc/robot/subsystems/WS2812.java
+++ src/main/java/frc/robot/subsystems/WS2812.java
@@ -19,107 +19,107 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  * Add your docs here.
  */
 public class WS2812 extends Subsystem {
-    private AddressableLED led;
-    private AddressableLEDBuffer ledBuffer;
+	private AddressableLED led;
+	private AddressableLEDBuffer ledBuffer;
 
-    // Store what the last hue of the first pixel is
-    private int rainbowFirstPixelHue;
-    
-    public WS2812() 
-    {
-        led = new AddressableLED(9); // TODO use constant to specify PWM port
+	// Store what the last hue of the first pixel is
+	private int rainbowFirstPixelHue;
+	
+	public WS2812() 
+	{
+		led = new AddressableLED(9); // TODO use constant to specify PWM port
 
-        // Reuse buffer
-        // Default to a length of 60, start empty output
-        // Length is expensive to set, so only set it once, then just update data
-        ledBuffer = new AddressableLEDBuffer(60);
+		// Reuse buffer
+		// Default to a length of 60, start empty output
+		// Length is expensive to set, so only set it once, then just update data
+		ledBuffer = new AddressableLEDBuffer(60);
 
-        led.setLength(ledBuffer.getLength());
+		led.setLength(ledBuffer.getLength());
 
-        // Set the data
-        led.setData(ledBuffer);
+		// Set the data
+		led.setData(ledBuffer);
 
-        led.start();
-    }
+		led.start();
+	}
 
-    @Override
-    public void initDefaultCommand() {  
-      // Set the default command for a subsystem here.
-      // setDefaultCommand(new MySpecialCommand());
-    }
-  
-    @Override
-    public void periodic() {
-      // Put code here to be run every loop
-    }
+	@Override
+	public void initDefaultCommand() {  
+		// Set the default command for a subsystem here.
+		// setDefaultCommand(new MySpecialCommand());
+	}
 
-    public void updateRainbow()
-    {
-      // For every pixel
-      for (var i = 0; i < ledBuffer.getLength(); i++) {
-  
-        // Calculate the hue - hue is easier for rainbows because the color
-        // shape is a circle so only one value needs to precess
-        final var hue = (rainbowFirstPixelHue + (i * 180 / ledBuffer.getLength())) % 180;
-  
-        // Set the value
-        ledBuffer.setHSV(i, hue, 255, 128);
-      }
-  
-      // Increase by to make the rainbow "move"
-      rainbowFirstPixelHue += 3;
-  
-      // Check bounds
-      rainbowFirstPixelHue %= 180; 
+	@Override
+	public void periodic() {
+		// Put code here to be run every loop
+	}
 
-      // Set the LEDs
-      led.setData(ledBuffer);    
-    }
+	public void updateRainbow()
+	{
+		// For every pixel
+		for (var i = 0; i < ledBuffer.getLength(); i++) {
 
-    public void updateHue(int hue)
-    {
-      // For every pixel
-      for (var i = 0; i < ledBuffer.getLength(); i++) {
-  
-        // Set the value
-        ledBuffer.setHSV(i, hue, 255, 128);
-      }
-  
-      // Set the LEDs
-      led.setData(ledBuffer);    
-    }
+			// Calculate the hue - hue is easier for rainbows because the color
+			// shape is a circle so only one value needs to precess
+			final var hue = (rainbowFirstPixelHue + (i * 180 / ledBuffer.getLength())) % 180;
 
-    public void updateRed()
-    {
-      updateHue(0);
-    }
+			// Set the value
+			ledBuffer.setHSV(i, hue, 255, 128);
+		}
 
-    public void updateYellow()
-    {
-      updateHue(30);
-    }
+		// Increase by to make the rainbow "move"
+		rainbowFirstPixelHue += 3;
 
-    public void updateGreen()
-    {
-      updateHue(60);
-    }
+		// Check bounds
+		rainbowFirstPixelHue %= 180; 
 
-    public void updateBlue()
-    {
-      updateHue(120);
-    }
+		// Set the LEDs
+		led.setData(ledBuffer);    
+	}
 
-    public void updateFromCamera(ICamera camera_in) {
-      ICamera camera = camera_in;
+	public void updateHue(int hue)
+	{
+		// For every pixel
+		for (var i = 0; i < ledBuffer.getLength(); i++) {
 
-      if (Math.abs(camera.getAngleToTurnToCompositeTarget()) < 5) { //Displays Green if in target
-        updateGreen();
-      }
-      else if (Math.abs(camera.getAngleToTurnToCompositeTarget()) < 10) { //displays yellow if close to target
-        updateYellow();
-      }
-      else { // displays red if far from target 
-        updateRed();
-      }
-    }
+			// Set the value
+			ledBuffer.setHSV(i, hue, 255, 128);
+		}
+
+		// Set the LEDs
+		led.setData(ledBuffer);    
+	}
+
+	public void updateRed()
+	{
+		updateHue(0);
+	}
+
+	public void updateYellow()
+	{
+		updateHue(30);
+	}
+
+	public void updateGreen()
+	{
+		updateHue(60);
+	}
+
+	public void updateBlue()
+	{
+		updateHue(120);
+	}
+
+	public void updateFromCamera(ICamera camera_in) {
+		ICamera camera = camera_in;
+
+		if (Math.abs(camera.getAngleToTurnToCompositeTarget()) < 5) { //Displays Green if in target
+			updateGreen();
+		}
+		else if (Math.abs(camera.getAngleToTurnToCompositeTarget()) < 10) { //displays yellow if close to target
+			updateYellow();
+		}
+		else { // displays red if far from target 
+			updateRed();
+		}
+	}
 }
