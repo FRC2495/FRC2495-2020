4f6ded9ecee67aae03583d6e184c8692d3664926
Mr. A <GabrielArtaud@comcast.net>
2020-01-24 21:09:15 -0500

    cleaned up and fixed a small bug

--- src/main/java/frc/robot/sensors/WS2812.java
+++ src/main/java/frc/robot/sensors/WS2812.java
@@ -11,6 +11,8 @@ import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import frc.robot.interfaces.ICamera;
 
+// see https://docs.wpilib.org/en/latest/docs/software/actuators/addressable-leds.html
+
 /**
  * Add your docs here.
  */
@@ -23,7 +25,7 @@ public class WS2812 {
     
     public WS2812() 
     {
-        led = new AddressableLED(9); // TODO use constant
+        led = new AddressableLED(9); // TODO use constant to specify PWM port
 
         // Reuse buffer
         // Default to a length of 60, start empty output
@@ -55,20 +57,16 @@ public class WS2812 {
         rainbowFirstPixelHue += 3;
     
         // Check bounds
-        rainbowFirstPixelHue %= 180;
-    
+        rainbowFirstPixelHue %= 180; 
 
         // Set the LEDs
         led.setData(ledBuffer);    
     }
 
-
-    public void updateBlue()
+    public void updateHue(int hue)
     {
         // For every pixel
         for (var i = 0; i < ledBuffer.getLength(); i++) {
-    
-          final var hue = 120;
     
           // Set the value
           ledBuffer.setHSV(i, hue, 255, 128);
@@ -80,71 +78,35 @@ public class WS2812 {
 
     public void updateRed()
     {
-        // For every pixel
-        for (var i = 0; i < ledBuffer.getLength(); i++) {
-    
-          final var hue = 0;
-    
-          // Set the value
-          ledBuffer.setHSV(i, hue, 255, 128);
-        }
-    
-        // Set the LEDs
-        led.setData(ledBuffer);    
+      updateHue(0);
+    }
+
+    public void updateYellow()
+    {
+      updateHue(30);
     }
 
     public void updateGreen()
     {
-        // For every pixel
-        for (var i = 0; i < ledBuffer.getLength(); i++) {
-    
-          final var hue = 60;
-    
-          // Set the value
-          ledBuffer.setHSV(i, hue, 255, 128);
-        }
-    
-        // Set the LEDs
-        led.setData(ledBuffer);    
+      updateHue(60);
+    }
+
+    public void updateBlue()
+    {
+      updateHue(120);
     }
 
     public void updateFromCamera(ICamera camera_in) {
         ICamera camera = camera_in;
-        if(Math.abs(camera.getAngleToTurnToCompositeTarget()) < 5){ //Displays Green if in target
-            for (var i = 0; i < ledBuffer.getLength(); i++) {
-    
-                final var hue = 60;
-          
-                // Set the value
-                ledBuffer.setHSV(i, hue, 255, 128);
-              }
-          
-              // Set the LEDs
-              led.setData(ledBuffer); 
+
+        if (Math.abs(camera.getAngleToTurnToCompositeTarget()) < 5) { //Displays Green if in target
+          updateGreen();
         }
-        else if(camera.getAngleToTurnToCompositeTarget() > 5 && camera.getAngleToTurnToCompositeTarget() < 10 ){ //displays yellow if close to target
-            for (var i = 0; i < ledBuffer.getLength(); i++) {
-    
-                final var hue = 30;
-          
-                // Set the value
-                ledBuffer.setHSV(i, hue, 100, 50);
-              }
-          
-              // Set the LEDs
-              led.setData(ledBuffer);  
+        else if (Math.abs(camera.getAngleToTurnToCompositeTarget()) < 10) { //displays yellow if close to target
+          updateYellow();
         }
-        else{ // displays red if far from target 
-            for (var i = 0; i < ledBuffer.getLength(); i++) {
-    
-                final var hue = 0;
-          
-                // Set the value
-                ledBuffer.setHSV(i, hue, 255, 128);
-              }
-          
-              // Set the LEDs
-              led.setData(ledBuffer);     
+        else { // displays red if far from target 
+          updateRed();
         }
     }
 }
